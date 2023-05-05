21d66d2115f243f1847b2de851a3efb54dc6fff3
MrBatata <mn.martinez@live.com.ar>
2023-05-05 09:45:50 -0300

    PWA with serviceworker

---
+++ README_1_PWA_convert.md
@@ -0,0 +1,42 @@
+# Generar funcionalidad PWA Progressive Web App en aplicación existente
+
+1.  `package.json` -> Instalar dependencias de Workbox:
+
+    "workbox-background-sync": "^6.5.4",
+    "workbox-broadcast-update": "^6.5.4",
+    "workbox-cacheable-response": "^6.5.4",
+    "workbox-core": "^6.5.4",
+    "workbox-expiration": "^6.5.4",
+    "workbox-google-analytics": "^6.5.4",
+    "workbox-navigation-preload": "^6.5.4",
+    "workbox-precaching": "^6.5.4",
+    "workbox-range-requests": "^6.5.4",
+    "workbox-routing": "^6.5.4",
+    "workbox-strategies": "^6.5.4",
+    "workbox-streams": "^6.5.4"
+
+    After copying dependencies in json, run command again to install packages -> `npm install`
+
+- NOTA: estas dependencias se instalan automáticamente con `npx create-react-app` con un template PWA. El comando sería: `npx create-react-app my-app --template cra-template-pwa`. Pero como la app en este caso queremos que sea existente, solo nos traemos las dependencias de esta app defecto.
+  Source: https://create-react-app.dev/docs/making-a-progressive-web-app/
+
+2.  Crear archivos de service worker: los crea también por defecto en el template PWA, los traemos del proyecto por defecto creado en el paso 1.
+    `service-worker.js`
+    `serviceWorkerRegistration.js`
+
+3.  Importar archivos SW en nuestro `index.js`:
+    On top: `import \* as serviceWorkerRegistration from './serviceWorkerRegistration';`
+    Below root.render(...): `serviceWorkerRegistration.register();`
+
+4.  Tenemos que tener la app en producción para probarla, no se pueden en desarrollo (`npm start`)
+    Para ello debemos formar la build y el server:
+    Ejecutar en consola:
+    `npm run build`
+    `serve -s build -l 5000`
+
+    O configurar un script que haga el proceso automático: npm run server-prod
+    Previo incluir en `package.json` -> `"server-prod": "npm run build && serve -s build -l 5000"`
+
+5.  Controlar en navegador que se haya creado el Service Worker en "Application" de la consola.
+6.  Se puede instalar la app de forma "nativa" en la PC o teléfono móvil.
+    La opción instalar aparece en la barra de búsqueda del explorador (ej. Google Chrome).
--- src/index.js
+++ src/index.js
@@ -3,33 +3,108 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 /*
-* Bootstrap imports
-*/
+ * Bootstrap imports
+ */
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 /*
-* Styles imports
+ * Styles imports
 ! Important: 
   Must go below Bootstrap, or will replace...
-*/
+ */
 import './index.css';
 
 /*
-* App import
-*/
+ * App import
+ */
 // import AppRoutingOne from './AppRoutingOne';
 // import App from './App';
 import AppRoutingFinal from './AppRoutingFinal';
 
+/*
+ * Import Service Worker
+ */
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
     {/* <App /> */}
     {/* <AppRoutingOne /> */}
     <AppRoutingFinal />
+    <hr className='my-3'></hr>
+    <button id='update-button'>Buscar e Instalar Update</button>
+    <button id='skip-waiting-button'>Activar Update</button>
+
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// Code provided by chatGPT to replace the npm package `@3m1/service-worker-updater`
+// This code adds functions to both buttons above: checks for SW updates and activates the update
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      const registration = await navigator.serviceWorker.register("/service-worker.js", { scope: "/" });
+      console.log("Service worker registered:", registration);
+
+      const updateButton = document.getElementById("update-button");
+      const skipWaitingButton = document.getElementById("skip-waiting-button");
+
+      updateButton.addEventListener("click", async () => {
+        await registration.update();
+        console.log("Service worker updated.");
+      });
+
+      skipWaitingButton.addEventListener("click", async () => {
+        if (registration.waiting) {
+          await registration.waiting.postMessage({ type: "SKIP_WAITING" });
+          console.log("Sent SKIP_WAITING message to service worker.");
+        };
+      });
+
+      if (registration.waiting) {
+        updateButton.disabled = false;
+        skipWaitingButton.disabled = false;
+      }
+
+      registration.addEventListener("updatefound", () => {
+        const newWorker = registration.installing;
+        newWorker.addEventListener("statechange", () => {
+          if (newWorker.state === "installed") {
+            if (navigator.serviceWorker.controller) {
+              updateButton.disabled = false;
+              skipWaitingButton.disabled = false;
+            } else {
+              console.log("Service worker installed for the first time.");
+            }
+          }
+        });
+      });
+
+      navigator.serviceWorker.addEventListener("controllerchange", () => {
+        window.location.reload();
+        console.log("Service worker controller changed.");
+      });
+
+      navigator.serviceWorker.addEventListener("message", event => {
+        if (event.data.type === "SKIP_WAITING_RESULT") {
+          window.location.reload();
+          console.log("Received SKIP_WAITING_RESULT message from service worker.");
+        }
+      });
+    } catch (err) {
+      console.error("Service worker registration failed:", err);
+    }
+  });
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
